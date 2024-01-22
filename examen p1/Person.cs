33b27a57ccc881f82e4ab220e204425045f35f6b
SammyUEO <samuel.pop@emanuel.ro>
2024-01-22 09:35:28 +0200

    examen p1

---
+++ examen p1/Person.cs
@@ -0,0 +1,41 @@
+﻿using examen_p1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen_p1
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public int YearOfBirth { get; set; }
+        public int Age
+        {
+            get
+            {
+              
+                return DateTime.Now.Year - YearOfBirth;
+            }
+        }
+
+    }
+}   static class PersonProcessor
+{
+    public static Person FindTheOldestPerson(Person[] people)
+    {
+        Person oldestPerson = null;
+
+        foreach (var person in people)
+        {
+            if (person == null || person.Age > oldestPerson.Age)
+            {
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+}
---
+++ examen p1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace examen_p1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cate persoane");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Person[] people = new Person[numberOfPeople];
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                Console.WriteLine("informatii");
+                people[i] = Print(people);
+                
+            }
+            foreach( var person in people)
+            {
+                Console.WriteLine($"Nume: {person.FirstName} {person.LastName}");
+                Console.WriteLine($"Year of birth: {person.YearOfBirth}");
+                Console.WriteLine($"Age: {person.Age}\n");
+            }
+
+            static Person Print(Person[] people)
+            {
+                Person person = new Person();
+                Console.WriteLine("Prenume: ");
+                person.FirstName = Console.ReadLine();
+
+                Console.WriteLine(" Nume de familie: ");
+                person.LastName = Console.ReadLine();
+
+                Console.WriteLine("Varsta: ");
+               person.YearOfBirth= int.Parse(Console.ReadLine());
+
+                
+
+                Person oldestPerson = PersonProcessor.FindTheOldestPerson(people);
+
+                if (oldestPerson != null)
+                {
+                    Console.WriteLine($"peroana cu cea mai mare varsta: {oldestPerson.FirstName};{oldestPerson.LastName}");
+                    Console.WriteLine($" varsta; {oldestPerson.Age}");
+                }
+                else
+                {
+                    Console.WriteLine("nu exista");
+                }
+
+                return person;
+            }
+           
+
+        }
+    }
+    
+}
